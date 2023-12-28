b3fd5c76388b02df2cea899919288c5dbc58f13d
jher235 <tim668666@gmail.com>
2023-12-28 22:33:18 +0900

    메이븐 설정 및 스프링 설정 완료,,,,

--- Spring/src/Spring/di/Program.java
+++ Spring/src/Spring/di/Program.java
@@ -1,6 +1,10 @@
 package Spring.di;
 
 import Spring.di.entity.Exam;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import Spring.di.entity.NewlecExam;
 import Spring.di.ui.ExamConsole;
 import Spring.di.ui.GridExamConsole;
@@ -10,11 +14,19 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		/*	스프링에게 지시하는 방법으로 코드를 변경 . XML파일을 이용함.
 		Exam exam = new NewlecExam();
-		ExamConsole console = new inlineExamConsole();// DI -> 인라인 이그젬 컨솔이 exam이라는 객체를 조립함
-		ExamConsole console2 = new GridExamConsole(exam);
+		
+		ExamConsole console = new GridExamConsole(exam);
 		console.setExam(exam);
-		console2.print();
+		*/
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+//		ExamConsole console = (ExamConsole) context.getBean("console");
+		//ExamConsole console = context.getBean(ExamConsole.class);
+		ExamConsole console = context.getBean(GridExamConsole.class);
+		console.print();
 	}
 
 }
