9a541b78d5732c1811bc699baaf13013fc9d8695
jher235 <tim668666@gmail.com>
2024-01-02 18:43:27 +0900

    AOP를 xml로 바꾸기

--- Spring/src/Spring/aop/Program.java
+++ Spring/src/Spring/aop/Program.java
@@ -4,6 +4,9 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import Spring.aop.entity.Exam;
 import Spring.aop.entity.NewlecExam;
 
@@ -11,31 +14,38 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		final Exam exam = new NewlecExam(1,1,1,1);
 		
-		Exam proxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), 
-				new Class[] {Exam.class},
-				new InvocationHandler() {
-					
-					@Override
-					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-						
-						long start = System.currentTimeMillis();
-						
-						Object result = method.invoke(exam, args);
-						
-						long end = System.currentTimeMillis();
-						
-						String message = (end - start) + "ms 시간이 걸렸습니다.";
-						System.out.println(message);
-
-						return result;
-					}
-				}
-				);
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/aop/setting.xml");
+		
+		Exam proxy = (Exam) context.getBean("exam");
 		
 		System.out.printf("total is %d\n", proxy.total());
 		System.out.printf("total is %f\n", proxy.avg());
+		//ApplicationContext context = new AnnotationConfigApplicationContext(NewlecDIConfig.class);
+		
+//		final Exam exam = new NewlecExam(1,1,1,1);
+//		
+//		
+//		Exam proxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), 
+//				new Class[] {Exam.class},
+//				new InvocationHandler() {
+//					
+//					@Override
+//					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//						
+//						long start = System.currentTimeMillis();
+//						
+//						Object result = method.invoke(exam, args);
+//						
+//						long end = System.currentTimeMillis();
+//						
+//						String message = (end - start) + "ms 시간이 걸렸습니다.";
+//						System.out.println(message);
+//
+//						return result;
+//					}
+//				}
+//				);
 		
 	}
 
