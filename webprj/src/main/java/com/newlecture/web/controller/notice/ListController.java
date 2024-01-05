a925a56e27588b547246214e1e10ee1530b2661c
jher235 <tim668666@gmail.com>
2024-01-05 14:19:04 +0900

    db연동 시도 중

--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -1,12 +1,23 @@
 package com.newlecture.web.controller.notice;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
+
 public class ListController implements Controller{
+	
+	private NoticeService noticeService;
+	
+	public void setNoticeService(NoticeService noticeserService) {
+		this.noticeService = noticeserService;
+	}
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -14,6 +25,8 @@ public class ListController implements Controller{
 //		ModelAndView mv = new ModelAndView("notice/list");
 		ModelAndView mv = new ModelAndView("notice.list");	//타일즈를 사용하기 위함.
 		
+		List<Notice> list = noticeService.getList(1, "TITLE", "");
+		mv.addObject("list",list);
 		
 		return mv;
 	}
