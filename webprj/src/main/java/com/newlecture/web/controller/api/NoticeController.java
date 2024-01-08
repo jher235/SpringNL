3fb87781b68f868c2001808cbfab8782a5b49b5a
jher235 <tim668666@gmail.com>
2024-01-08 11:57:22 +0900

    querystring을 받어오는 requestparam 사용중

--- webprj/src/main/java/com/newlecture/web/controller/api/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/api/NoticeController.java
@@ -3,14 +3,18 @@ package com.newlecture.web.controller.api;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
 
-@RestController("apiNoticeController")//이름을 이렇게 지정해주면 같은 파일일때 오류가 안남
+@RestController("apiNoticeController")//이름을 이렇게 지정해주면 동일한 파일명이 있어도 오류가 안남
 @RequestMapping("/api/notice/")
 public class NoticeController {
 	
@@ -18,8 +22,9 @@ public class NoticeController {
 	private NoticeService service;
 	
 	@RequestMapping("list")
-	public Notice list() throws ClassNotFoundException, SQLException {
-		
+	public Notice list(@RequestParam(name="p", defaultValue = "1") String page) throws ClassNotFoundException, SQLException {
+		//String p = request.getParameter("p");
+		System.out.println("page: "+page);
 		
 		List<Notice> list = service.getList(1, "TITLE", "");
 		
